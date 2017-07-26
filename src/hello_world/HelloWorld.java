75015e11b38253491f73050404b051bc177ff581
z2130022 <z2130022@user-PC>
2017-07-26 15:57:27 +0900

    最初のコミット

---
+++ src/hello_world/HelloWorld.java
@@ -0,0 +1,17 @@
+package hello_world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+
+		int i = 1;
+		while(i <= 100){
+			String a = String.valueOf(i);
+			System.out.println(a);
+			i++;
+		}
+
+	}
+}
